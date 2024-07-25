03547e07337bffb1ac346f8894638a87146d5f9c
RajThangu <60980572+RajThangu@users.noreply.github.com>
2024-07-25 12:35:53 +0530

    Initial commit

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+public class Pattern
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <=3; i=i+1)
+        {
+            for (int j = 1; j<=3; j=j+1)
+            {
+                System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+}
